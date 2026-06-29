49dbf8b91bb0a9abbc734b40a7b1c50f5aec62bc
Nithin Pillalamarri <nithinpillalamarri31429@gmail.com>
2026-06-29 12:15:43 +0530

    Remove Github buttons

--- src/components/sections/ProjectsSection.tsx
+++ src/components/sections/ProjectsSection.tsx
@@ -154,7 +154,7 @@ function ProjectCard({ project, index, inView }: ProjectCardProps) {
         </div>
 
         {/* Buttons */}
-        <div className="flex gap-2 pt-4 border-t border-border/40">
+       <div className="flex justify-center pt-4 border-t border-border/40">
          <a
   href={project.pdf}
   target="_blank"
@@ -164,13 +164,7 @@ function ProjectCard({ project, index, inView }: ProjectCardProps) {
   <ExternalLink size={12} />
   View Project Report
 </a>
-          <a
-            href="#"
-            className="flex items-center gap-1.5 px-4 py-2 text-xs font-medium bg-white/5 hover:bg-white/10 text-muted-foreground hover:text-foreground rounded-lg transition-all border border-border/40"
-          >
-            <Github size={12} />
-            GitHub
-          </a>
+         
         </div>
       </div>
     </motion.div>
